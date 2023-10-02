6c09c5f77055f9d42f159cc059586f44b35ffcce
Addin Munawwar <moonawar@protonmail.com>
2023-10-02 21:44:34 +0700

    simple routing setup

--- src/app/index.php
+++ src/app/index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> Haloo </h1>
-</body>
-</html>
+<?php
+    require_once __DIR__ . "/main/Main.php";
+
+    $main = new Main();
---
+++ src/app/main/Main.php
@@ -0,0 +1,7 @@
+<?php
+class Main {
+    public function __construct()
+    {
+        require_once __DIR__ . '/../pages/Example.php';
+    }
+}
