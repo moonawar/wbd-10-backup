658fd652cdff758c71db62704905e519fe1e4ea9
Alexander Jason <alexanderjason526@gmail.com>
2023-10-05 21:31:31 +0700

    feat: add pop up

--- app/pages/admin/AddGenrePage.php
+++ app/pages/admin/AddGenrePage.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/globals.css">
     <!-- Navbar CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/navbar.css">
+    <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/popup.css">
     <!-- Page-specific CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/admin/crud-page.css">
     <title>Add Genre</title>
@@ -18,19 +19,63 @@
 <body>
     <!-- Navigation bar -->
     <?php include(dirname(__DIR__) . '../../components/Navbar.php') ?>
-    <div class="container">
-        <div class="content">
-            <h2>Add Genre Page</h2>
-            <form class="form-box center-contents">
-                <div class="form-content flex-column"> 
-                    <label class="form-label" for="genre">Genre:</label>
-                    <input class="form-field" type="text" id="genre" name="genre" required>
+    <section>
+        <!-- Pop Up -->
+        <span class="overlay"></span>
 
-                    <input type="submit" class="button green-button" value="Add">
-                </div>
-            </form>
+        <div class="modal-box">
+            <h2>Add Genre</h2>
+            <h3>Are you sure want to add this genre?</h3>
+
+            <div class="buttons">
+                <button class="cancel-btn">Cancel</button>
+                <button class="confirm-btn">Add</button>
+            </div>
         </div>
-    </div>
+        <div class="container">
+            <div class="content">
+                <h2>Add Genre Page</h2>
+                <form class="form-box center-contents">
+                    <div class="form-content flex-column">
+                        <label class="form-label" for="genre">Genre:</label>
+                        <input class="form-field" type="text" id="genre" name="genre" required>
+                        <input type="button" class="show-modal button green-button" value="Add">
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+    <script>
+    const section = document.querySelector("section"),
+        overlay = document.querySelector(".overlay"),
+        showBtn = document.querySelector(".show-modal"),
+        closeBtn = document.querySelector(".cancel-btn");
+
+    function showModal() {
+        const inputFields = document.querySelectorAll('.form-field');
+
+        // Check if any input field is empty
+        const isEmpty = Array.from(inputFields).some(field => field.value.trim() === '');
+
+        if (!isEmpty) {
+            section.classList.add("active");
+        } else {
+            alert("Please fill in all the required fields.");
+            showBtn.removeEventListener("click", showModal);
+        }
+    }
+
+    showBtn.addEventListener("click", showModal);
+
+    overlay.addEventListener("click", () =>
+        section.classList.remove("active")
+    );
+
+    closeBtn.addEventListener("click", () =>
+        section.classList.remove("active")
+    );
+</script>
 </body>
 
 </html>
