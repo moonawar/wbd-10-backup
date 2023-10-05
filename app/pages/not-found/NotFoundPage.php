c1a4eeb32da447bb7eb125c3dcdba1c73fb532c2
Alexander Jason <alexanderjason526@gmail.com>
2023-10-05 17:00:46 +0700

    fix: add navbar css

--- app/pages/not-found/NotFoundPage.php
+++ app/pages/not-found/NotFoundPage.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" sizes="180x180" href="<?= BASE_URL ?>/images/icon/favicon-110.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/images/icon/favicon-32.png">
+    <link rel="icon" sizes="180x180" href="<?= BASE_URL ?>/icon/favicon-110.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/icon/favicon-32.png">
     <!-- Global CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/globals.css">
+    <!-- Navbar CSS -->
+    <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/template/navbar.css">
     <!-- Page-specific CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/not-found/not-found.css">
     <title>Page not found!</title>
