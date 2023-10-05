431318aed2d2cad4dff0f457f8b90ac3924005fa
Alexander Jason <alexanderjason526@gmail.com>
2023-10-05 18:12:41 +0700

    feat: add admin controller

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,29 @@
+<?php
+
+class AdminController extends Controller implements ControllerInterface
+{
+    public function index()
+    {
+        $notFoundView = $this->view('not-found', 'NotFoundView');
+        $notFoundView->render();
+    }
+
+    public function addBook() 
+    {
+        // $userModel = $this->model('UserModel');
+        // $result = $userModel->getAllUsers();
+
+        $addBookView = $this->view('admin', 'AddBookView');
+        $addBookView->render();
+    }
+
+    public function addAuthor() {
+        $addAuthorView = $this->view('admin', 'AddAuthorView');
+        $addAuthorView->render();   
+    }
+
+    public function addGenre() {
+        $addGenreView = $this->view('admin', 'AddGenreView');
+        $addGenreView->render();   
+    }
+}
